7c780d48ddb060e24fd3c7c7417814a6ed1a7c09
Mark Wyse <muwyse@gmail.com>
2020-09-22 16:32:52 -0700

    WiP: ME testbench

--- bp_me/test/tb/bp_cce/wrapper.v
+++ bp_me/test/tb/bp_cce/wrapper.v
@@ -18,6 +18,7 @@ module wrapper
    `declare_bp_proc_params(bp_params_p)
 
    // interface widths
+   `declare_bp_lce_cce_if_header_widths(cce_id_width_p, lce_id_width_p, paddr_width_p, lce_assoc_p)
    `declare_bp_lce_cce_if_widths(cce_id_width_p, lce_id_width_p, paddr_width_p, lce_assoc_p, cce_block_width_p)
    `declare_bp_mem_if_widths(paddr_width_p, cce_block_width_p, lce_id_width_p, lce_assoc_p, cce_mem)
    , localparam cfg_bus_width_lp = `bp_cfg_bus_width(vaddr_width_p, core_id_width_p, cce_id_width_p, lce_id_width_p, cce_pc_width_p, cce_instr_width_p)
@@ -36,26 +37,47 @@ module wrapper
    , output [cce_instr_width_p-1:0]                        ucode_data_o
 
    // LCE-CCE Interface
-   , input [lce_cce_req_width_lp-1:0]                      lce_req_i
-   , input                                                 lce_req_v_i
-   , output logic                                          lce_req_yumi_o
+   // BP Burst protocol: ready&valid
+   // TODO: correct data width for mem_cmd/resp networks. Parameter?
+   , input [lce_cce_req_header_width_lp-1:0]           lce_req_header_i
+   , input                                             lce_req_header_v_i
+   , output logic                                      lce_req_header_ready_o
+   , input [dword_width_p-1:0]                         lce_req_data_i
+   , input                                             lce_req_data_v_i
+   , output logic                                      lce_req_data_ready_o
 
-   , input [lce_cce_resp_width_lp-1:0]                     lce_resp_i
-   , input                                                 lce_resp_v_i
-   , output logic                                          lce_resp_yumi_o
+   , input [lce_cce_resp_header_width_lp-1:0]          lce_resp_header_i
+   , input                                             lce_resp_header_v_i
+   , output logic                                      lce_resp_header_ready_o
+   , input [dword_width_p-1:0]                         lce_resp_data_i
+   , input                                             lce_resp_data_v_i
+   , output logic                                      lce_resp_data_ready_o
 
-   , output logic [lce_cmd_width_lp-1:0]                   lce_cmd_o
-   , output logic                                          lce_cmd_v_o
-   , input                                                 lce_cmd_ready_i
+   // ready->valid
+   , output logic [lce_cmd_header_width_lp-1:0]        lce_cmd_header_o
+   , output logic                                      lce_cmd_header_v_o
+   , input                                             lce_cmd_header_ready_i
+   , output logic [dword_width_p-1:0]                  lce_cmd_data_o
+   , output logic                                      lce_cmd_data_v_o
+   , input                                             lce_cmd_data_ready_i
 
    // CCE-MEM Interface
-   , input [cce_mem_msg_width_lp-1:0]                      mem_resp_i
-   , input                                                 mem_resp_v_i
-   , output logic                                          mem_resp_yumi_o
+   // BP Burst protocol: ready&valid
+   // TODO: correct data width for mem_cmd/resp networks. Parameter?
+   , input [cce_mem_msg_header_width_lp-1:0]           mem_resp_header_i
+   , input                                             mem_resp_header_v_i
+   , output logic                                      mem_resp_header_ready_o
+   , input [dword_width_p-1:0]                         mem_resp_data_i
+   , input                                             mem_resp_data_v_i
+   , output logic                                      mem_resp_data_ready_o
 
-   , output logic [cce_mem_msg_width_lp-1:0]               mem_cmd_o
-   , output logic                                          mem_cmd_v_o
-   , input                                                 mem_cmd_ready_i
+   , output logic [cce_mem_msg_header_width_lp-1:0]    mem_cmd_header_o
+   , output logic                                      mem_cmd_header_v_o
+   , input                                             mem_cmd_header_ready_i
+   , output logic [dword_width_p-1:0]                  mem_cmd_data_o
+   , output logic                                      mem_cmd_data_v_o
+   , input                                             mem_cmd_data_ready_i
+
   );
 
   bp_cce_wrapper
